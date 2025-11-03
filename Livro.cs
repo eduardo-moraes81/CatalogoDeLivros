937aad11ceaeebd70d4ba95a5813ccbd897e4b85
Eduardo Moraes <eduardoaraujo1423@gmail.com>
2025-11-03 17:53:14 -0300

    conexao com banco de dados

---
+++ Livro.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogoDeLivros
+{
+    public class Livro
+    {
+        public int Id { get; set; }
+        public string? Titulo { get; set; }
+        public string? Autor { get; set; }
+        public int AnoPublicacao { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,36 @@
+using CatalogoDeLivros;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data Source=livros.db"));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/catalogoDeLivros", () =>
+{
+
+});
+
+app.MapPost("/catalogoDeLivros", () =>
+{
+
+});
+
+app.MapPut("/catalogoDeLivros/{id}", (int id, string novoNome) =>
+{
+
+});
+
+app.MapPatch("/catalogoDeLivros/{id}", (int id, string campo, string valor) =>
+{
+
+});
+
+app.MapDelete("/catalogoDeLivros/{id}", (int id) =>
+{
+
+});
 
 app.Run();
